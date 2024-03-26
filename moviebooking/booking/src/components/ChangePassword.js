25941e3be58d89cbd980c648903670fa6ac449e4
Parker Patterson <plp40011@uga.edu>
2024-03-26 16:17:12 -0400

    Not finished

---
+++ moviebooking/booking/src/components/ChangePassword.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ChangePassword = () => {
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (newPassword !== confirmNewPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+        // API call to change the password would go here
+        alert("Your password has been changed successfully!");
+        // Redirect to login page or another appropriate page
+    };
+
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-body" style={{ backgroundColor: '#AEC3B0' }}>
+                            <h5 className="card-title text-center">Change Password</h5>
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-group">
+                                    <label htmlFor="newPassword">New Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="newPassword"
+                                        value={newPassword}
+                                        onChange={(e) => setNewPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="confirmNewPassword">Confirm New Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="confirmNewPassword"
+                                        value={confirmNewPassword}
+                                        onChange={(e) => setConfirmNewPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <button type="submit" className="btn btn-primary btn-block mt-4">
+                                    Submit
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default ChangePassword;
