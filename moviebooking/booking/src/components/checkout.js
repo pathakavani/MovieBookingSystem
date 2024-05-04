9d3d6f5162e54fe69bb882bf50d81ad9270b6834
SaiSree00 <saisreevani059@gmail.com>
2024-05-04 11:45:41 -0400

    checkout page styling

--- moviebooking/booking/src/components/checkout.js
+++ moviebooking/booking/src/components/checkout.js
@@ -261,23 +261,35 @@ function OrderPage() {
         </nav>
         <div className="orderbox">
           <div className="orderdetails">
-            <p style={{ display: 'inline-block' }}><b>Order Details:</b></p>
+            <p style={{display: 'inline-block'}}><b>Order Details:</b></p>
             <h1><b>{movie}</b></h1>
             <br/>
             <p>Tickets:</p>
             <p>Movie (Adult) x {adults} - ${costs.tadults}</p>
-            <br />
+            <br/>
             <p>Movie (Child) x {children} - ${costs.tchildren}</p>
-            <br />
+            <br/>
             <p>Movie (Seniors) x {children} - ${costs.tsenior}</p>
-            <br />
+            <br/>
             <p>Subtotal - ${costs.sub}</p>
-            <br />
+            <br/>
             <p>Tax - ${costs.tax.toFixed(2)}</p>
-            <br />
+            <br/>
             <p>Total - ${costs.total.toFixed(2)}</p>
           </div>
           <div className="rightbox">
+            <div className="manage-promotions">
+              {/*className="manage-promotions-container"*/}
+              <button onClick={togglePromoForm}>Apply Promo</button>
+              {showPromoForm && (
+                  <form>
+                    <label htmlFor="promo" style={{marginTop: '50px'}}>Promo Code</label>
+                    <input type="text" id="promo" name="promo" placeholder="Enter your promo code here..."/>
+                    <button>Apply</button>
+                    {/*  implement on click*/}
+                  </form>
+              )}
+            </div>
             <div className="paymentmethodsgrid">
               <button className='paymentmethod'>Existing Payment Info</button>
               <button className='paymentmethod' onClick={togglePaymentForm}>Payment Method</button>
@@ -287,71 +299,63 @@ function OrderPage() {
                   //   {/* Payment details form */}
                   // </
                   <div className='paymentform'>
-                  <form id="paymentForm" onSubmit={handleSubmit}>
-                    <h2>Payment Details</h2>
+                    <form id="paymentForm" onSubmit={handleSubmit}>
+                      <h2>Payment Details</h2>
 
-                    <div className="form-group">
-                      {/*<label htmlFor="cardName">Name on Card</label>*/}
-                      <input type="text" id="cardName" name="cardName" placeholder="Name on Card" required/>
-                    </div>
-
-                    <div className="form-group">
-                      {/*<label htmlFor="cardNumber">Card Number</label>*/}
-                      <input type="text" id="cardNumber" name="cardNumber" placeholder="Card Number"
-                             pattern="\d{16}" required/>
-                    </div>
-
-                    <div className="form-row">
                       <div className="form-group">
-                        {/*<label htmlFor="cardExpiry">Expiry Date</label>*/}
-                        <input type="text" id="cardExpiry" name="cardExpiry" placeholder="Expiry Date(MM/YY)" pattern="\d{2}/\d{2}"
-                               required/>
+                        {/*<label htmlFor="cardName">Name on Card</label>*/}
+                        <input type="text" id="cardName" name="cardName" placeholder="Name on Card" required/>
                       </div>
 
                       <div className="form-group">
-                        {/*<label htmlFor="cardCVC">CVC</label>*/}
-                        <input type="text" id="cardCVC" name="cardCVC" placeholder="CVC" pattern="\d{3}" required/>
+                        {/*<label htmlFor="cardNumber">Card Number</label>*/}
+                        <input type="text" id="cardNumber" name="cardNumber" placeholder="Card Number"
+                               pattern="\d{16}" required/>
                       </div>
-                    </div>
 
-                    <div className="form-group">
-                      <input type="checkbox" id="saveCard" name="saveCard"/>
-                      <label htmlFor="saveCard">Save this card for future payments</label>
-                    </div>
+                      <div className="form-row">
+                        <div className="form-group">
+                          {/*<label htmlFor="cardExpiry">Expiry Date</label>*/}
+                          <input type="text" id="cardExpiry" name="cardExpiry" placeholder="Expiry Date(MM/YY)"
+                                 pattern="\d{2}/\d{2}"
+                                 required/>
+                        </div>
 
-                    <button type="submit">Submit Payment</button>
-                  </form>
+                        <div className="form-group">
+                          {/*<label htmlFor="cardCVC">CVC</label>*/}
+                          <input type="text" id="cardCVC" name="cardCVC" placeholder="CVC" pattern="\d{3}" required/>
+                        </div>
+                      </div>
+
+                      <div className="form-group">
+                        <input type="checkbox" id="saveCard" name="saveCard"/>
+                        <label htmlFor="saveCard">Save this card for future payments</label>
+                      </div>
+
+                      <button type="submit">Submit Payment</button>
+                    </form>
                   </div>
               )}
             </div>
-            <div>
-            <button  onClick={togglePromoForm}>Apply Promo</button>
-            {showPromoForm && (
-                <form>
-                  <label htmlFor="promo" style={{marginTop: '70px'}}>Promo Code</label>
-                  <input type="text" id="promo" name="promo" placeholder="Enter your promo code here..."/>
-                  <button>Apply</button>
-                {/*  implement on click*/}
-                </form>
-            )}
-            </div>
+
             {/*<form>*/}
             {/*  <label htmlFor="promo" style={{marginTop: '70px'}}>Promo Code</label>*/}
             {/*  <input type="text" id="promo" name="promo" placeholder="Enter your promo code here..."/>*/}
             {/*</form>*/}
-            <div className="confirmcancelbuttons">
-              <button
-                  style={{backgroundColor: 'white', width: '120px', height: '40px', borderRadius: '5px', color: 'black'}}
-                  onClick={(e) => handleLogout(e)}>Previous
-              </button>
-              <button
-                  style={{backgroundColor: 'white', width: '120px', height: '40px', borderRadius: '5px', color: 'black'}}
-                  onClick={(e) => handleLogout(e)}>Cancel
-              </button>
-              <button style={{backgroundColor: 'white', width: '120px', height: '40px', borderRadius: '5px'}}><a
-                  href="ConfirmationPage">Confirm Order</a></button>
-            </div>
+
           </div>
+        </div>
+        <div className="confirmcancelbuttons">
+          <button
+              style={{backgroundColor: 'white', width: '120px', height: '40px', borderRadius: '5px', color: 'black'}}
+              onClick={(e) => handleLogout(e)}>Previous
+          </button>
+          <button
+              style={{backgroundColor: 'white', width: '120px', height: '40px', borderRadius: '5px', color: 'black'}}
+              onClick={(e) => handleLogout(e)}>Cancel
+          </button>
+          <button style={{backgroundColor: 'white', width: '120px', height: '40px', borderRadius: '5px'}}><a
+              href="ConfirmationPage">Confirm Order</a></button>
         </div>
       </div>
   );
